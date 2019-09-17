1082768a66f1a14f928d5ee681dbe02bdc52409a
saja amer <sajaamer86@gmail.com>
2019-09-17 09:51:35 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-function addingEventListener() {
-}
+ /*function addingEventListener(event) {
+    document.getElementById('event');
+    event.addEventListener('click', function(event) {
+  alert('I was clicked!');
+});
+  } */
+  function addingEventListener(event){
+  const input = document.getElementById('input');
+   input.addEventListener('click', function(event) {
+  alert('I was clicked!');
+   });
+    
+  }
