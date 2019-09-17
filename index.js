089a1c912666a548b6a537f379658764ab878c01
saja amer <sajaamer86@gmail.com>
2019-09-17 09:43:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
-function addingEventListener() {
-}
+  function addingEventListener(event) {
+    document.getElementById('event');
+    event.addEventListener('click', function(event) {
+  alert('I was clicked!');
+});
+  }
